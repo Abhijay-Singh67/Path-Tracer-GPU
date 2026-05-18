b49fcee1b2ef7f4a314f01113a8774b769a8400e
Abhijay-Singh67 <abhijaysingh.1011@gmail.com>
2026-05-18 22:32:04 +0530

    Just made some final renders. The project is marked complete...for now :)

--- src/main.cpp
+++ src/main.cpp
@@ -37,7 +37,7 @@ float focus_dist = 3.4f;
 //For saving images
 bool saveRequested = false;
 //For Rendering
-glm::vec4 background_color = glm::vec4(0.53, 0.81, 0.92, 1.0);// xyz = color, w = intensity;
+glm::vec4 background_color = glm::vec4(0.0f);// xyz = color, w = intensity;
 float exposure = 1.0f;
 float envIntensity = 1.0f;
 
@@ -206,41 +206,168 @@ int main() {
 
     // ===============================================SCENE========================================================
 
-    materials.clear();
+    // ============================================================
+// SIMPLE CORNELL BOX
+// ============================================================
+
+materials.clear();
 spheres.clear();
 quads.clear();
 vertices.clear();
 indices.clear();
 mediumSpheres.clear();
 
-// 0: large light grey ground plane (matte)
-materials.push_back({glm::vec4(0.8f, 0.8f, 0.8f, 0.0f), glm::vec4(0.0f), glm::vec4(0.0f)});
+// ============================================================
+// MATERIALS
+// ============================================================
 
-// 1: red glass bunny
+// 0 --- white
 materials.push_back({
-    glm::vec4(1.0f, 1.0f, 1.0f, 2.0f),
-    glm::vec4(0.0f, 1.5f, 0.0f, 0.0f),
-    glm::vec4(0.15f, 1.5f, 1.8f, 0.0f)
+    glm::vec4(0.73f, 0.73f, 0.73f, 0.0f),
+    glm::vec4(0.0f),
+    glm::vec4(0.0f)
 });
 
-// Big ground plane
-auto makeQuad = [](glm::vec3 Q, glm::vec3 u, glm::vec3 v, int mat) {
+// 1 --- red wall
+materials.push_back({
+    glm::vec4(0.65f, 0.05f, 0.05f, 1.0f),
+    glm::vec4(0.0f),
+    glm::vec4(0.0f)
+});
+
+// 2 --- green wall
+materials.push_back({
+    glm::vec4(0.12f, 0.45f, 0.15f, 1.0f),
+    glm::vec4(0.0f),
+    glm::vec4(0.0f)
+});
+
+// 3 --- ceiling light
+materials.push_back({
+    glm::vec4(1.0f, 1.0f, 1.0f, 3.0f),
+    glm::vec4(0.0f, 0.0f, 20.0f, 0.0f),
+    glm::vec4(0.0f)
+});
+
+//4 --- chess
+materials.push_back({
+    glm::vec4(0.73f, 0.73f, 0.73f, 0.0f),
+    glm::vec4(0.0f),
+    glm::vec4(0.0f)
+});
+
+//5 - red tinted glass
+materials.push_back({
+    glm::vec4(1.0f, 0.35f, 0.35f, 2.0f),
+    glm::vec4(0.0f, 1.5f, 0.0f, 0.0f),
+    glm::vec4(0.12f, 1.0f, 1.2f, 0.0f)
+});
+
+unsigned int chess = textures.load("src\\chess.jpg");
+materials[4].extra.w = chess;
+
+// ============================================================
+// HELPERS
+// ============================================================
+
+auto makeQuad =
+[](glm::vec3 Q,
+   glm::vec3 u,
+   glm::vec3 v,
+   int mat)
+{
     GPUQuad q;
+
     q.Q = glm::vec4(Q, float(mat));
     q.u = glm::vec4(u, 0.0f);
     q.v = glm::vec4(v, 0.0f);
+
     return q;
 };
 
-quads.push_back(makeQuad({-50.0f, -3.0f, -50.0f}, {100.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 100.0f}, 0));
+// ============================================================
+// BOX DIMENSIONS
+// ============================================================
 
-// Bunny
-Mesh bunny;
-bunny.loadOBJ("src\\Bunny.obj");
+const float W = 15.0f;
+const float H = 15.0f;
+const float D = 15.0f;
+
+// ============================================================
+// FLOOR
+// ============================================================
+
+quads.push_back(makeQuad(
+    glm::vec3(-W, -H, -D),
+    glm::vec3( 2.0f * W, 0.0f, 0.0f),
+    glm::vec3( 0.0f, 0.0f,-2.0f * D),
+    4
+));
+
+// ============================================================
+// CEILING
+// ============================================================
+
+quads.push_back(makeQuad(
+    glm::vec3(-W, H, -D),
+    glm::vec3( 2.0f * W, 0.0f, 0.0f),
+    glm::vec3( 0.0f, 0.0f,-2.0f * D),
+    3
+));
+
+// ============================================================
+// BACK WALL
+// ============================================================
+
+quads.push_back(makeQuad(
+    glm::vec3(-W, -H, -2.0f * D),
+    glm::vec3( 2.0f * W, 0.0f, 0.0f),
+    glm::vec3( 0.0f, 2.0f * H, 0.0f),
+    0
+));
+
+// ============================================================
+// LEFT WALL (RED)
+// ============================================================
+
+quads.push_back(makeQuad(
+    glm::vec3(-W, -H, -D),
+    glm::vec3(0.0f, 2.0f * H, 0.0f),
+    glm::vec3(0.0f, 0.0f,-2.0f * D),
+    1
+));
+
+// ============================================================
+// RIGHT WALL (GREEN)
+// ============================================================
+
+quads.push_back(makeQuad(
+    glm::vec3(W, -H, -D),
+    glm::vec3(0.0f, 2.0f * H, 0.0f),
+    glm::vec3(0.0f, 0.0f,-2.0f * D),
+    2
+));
+
+// ============================================================
+// CEILING LIGHT
+// ============================================================
+
+quads.push_back(makeQuad(
+    glm::vec3(-1.5f, H - 0.01f, -6.5f),
+    glm::vec3(3.0f, 0.0f, 0.0f),
+    glm::vec3(0.0f, 0.0f,-3.0f),
+    3
+));
+
+Mesh mesh;
+mesh.loadOBJ("knight.obj");
 glm::mat4 transform = glm::mat4(1.0f);
-transform = glm::translate(transform, glm::vec3(0.0f, -3.0f, -6.0f));
-transform = glm::scale(transform, glm::vec3(15.0f));
-bunny.appendToScene(vertices, indices, 1, transform);
+transform = glm::translate(transform, glm::vec3(-3.0f, -15.0f, -3.0f));
+transform = glm::rotate(transform, glm::radians(-90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+transform = glm::rotate(transform, glm::radians(30.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+mesh.appendToScene(vertices, indices, 5, transform);
+
+
 
     //================================================BVH GENERATION FOR THE SCENE=============================================
     //Generating the BVH for the Scene
